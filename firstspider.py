39117b489709eca6b4d5e32ad216be8b436d88c4
user-amali <70620939+user-amali@users.noreply.github.com>
2021-02-24 16:29:52 +1100

    Add files via upload

--- firstspider.py
+++ firstspider.py
@@ -8,6 +8,7 @@
 import scrapy
 import re
 
+
 class cataloguespider(scrapy.Spider):
     name = "cataloguespider"
     start_urls = [
@@ -21,22 +22,27 @@ class cataloguespider(scrapy.Spider):
             cat_list = []
             # Get the page title from breadcrumbs. 
             # The previous page titles need to be appended to get the current full title.
-            for categoryname in response.css('a.breadcrumb::text').getall():
-                cat_list.append(categoryname)
-                cat_list.append('/')
-            category = ''.join(cat_list)
+            # for categoryname in response.css('a.breadcrumb::text').getall():
+            #    cat_list.append(categoryname)
+            #    cat_list.append('/')
+            # Append the last item to the title
+            #category = ''.join(cat_list)
+         
             
             # Get category from the title for the webpage
             # Also, get websitename, url, and description for the website links
             # listed on the response webpage
             for site in response.css('div.site-item'):
+                # get categorytitle and remove leading '/desc/'
+                categorytitle = response.css('div.desc-and-faq a::attr(href)').get()
+                categorytitle = re.sub(r"^/desc/", " ", categorytitle)
                 # get description of the website for further processing
                 description = site.css('div.title-and-desc div.site-descr::text').get()
                 # Truncate leading and trailing whitespaces using regular expressions
                 # as well as remove any other characters
-                description = re.sub(r"^\s+|\s+$", " ", description)
+                description = re.sub(r"^\s+|\s+$", " ", description)                
                 yield{
-                    'Category': category,
+                    'Category': categorytitle,
                     'WebsiteName': site.css('div.title-and-desc div.site-title::text').get(),
                     'WebsiteURL': site.css('div.title-and-desc a::attr(href)').get(),
                     'Description': description
@@ -50,6 +56,6 @@ class cataloguespider(scrapy.Spider):
         yield from response.follow_all(anchors, callback=self.parse)
     
 
-           
+
                 
                 
