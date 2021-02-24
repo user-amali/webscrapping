81fd9bfb6fb97293de9ed8728bf30273f9722cac
user-amali <70620939+user-amali@users.noreply.github.com>
2021-02-24 11:03:48 +1100

    Updated with some changes

--- firstspider.py
+++ firstspider.py
@@ -1,3 +1,10 @@
+# This program crawls subcategory (and its subcategories) from the domain http://dmoztools.net. 
+# The dmoztools.net is the directory of webpages for different categories.
+# At this stage, it will crawl dmoztools.net/computer/freeware and its subcategories for (web)sites 
+# listed on each of the pages. This program can run until it has crawled all the pages unless 
+# the spider is stopped by Ctrl+C. As this can cause too much traffic, further versions of this program
+# will have extensions/spider settings to stop the spider when it reaches a certain limit.
+
 import scrapy
 import re
 
@@ -16,7 +23,7 @@ class cataloguespider(scrapy.Spider):
                 cat_list.append(categoryname)
                 cat_list.append('/')
             category = ''.join(cat_list)
-            re.sub(r"^\s+|\s+$", "", category, sep='')
+            re.sub(r"^\s+|\s+$", "", category)
             
             for site in response.css('div.site-item'):
                 yield{
